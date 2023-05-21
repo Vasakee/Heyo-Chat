a99a5056b9992198c361b5bdca9139f996e91aab
Basil <basildayigil@gmail.com>
2023-05-22 00:20:06 +0100

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const PORT = process.env.PORT || 4000
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:3001'
+        origin: 'https://heyo-chat.onrender.com'
     },
 })
 
